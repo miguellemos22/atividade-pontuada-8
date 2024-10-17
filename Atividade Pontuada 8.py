eb9269808cdad2294b0c9479c652234bd9b4140a
GabrielDV2004 <gabrielsantosgpt04@gmail.com>
2024-10-17 19:47:42 +0000

    salvar

--- Atividade Pontuada 8.py
+++ Atividade Pontuada 8.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
 # Criando banco de dados.
 MEU_BANCO = create_engine("sqlite:///meubanco.db")
 
@@ -15,7 +16,7 @@ session = Session()
 Base = declarative_base()
 
 class Usuario(Base):
-    __tablename__ = "usuarios"
+    __tablename__ = "funcionarios"
 
     # Definindo campos da tabela.
     id = Column("id", Integer, primary_key=True, autoincrement=True)
@@ -42,4 +43,71 @@ class Usuario(Base):
 Base.metadata.create_all(bind=MEU_BANCO)
 
 # Salvar no banco de dados.
-os.system("cls || clear")        
+os.system("cls || clear")
+
+
+# Funções CRUD
+def create_funcionario(session, nome, idade, cpf, setor, funcao, salario, telefone):
+    novo_funcionario = funcionario(nome=nome, idade=idade, cpf=cpf, setor=setor, funcao=funcao, salario=salario, telefone=telefone)
+    session.add(novo_funcionario)
+    session.commit()
+
+def salvar_funcionario(session, funcionario_id):
+    funcionario = session.query(funcionario).filter_by(id=funcionario_id).first()
+    if funcionario:
+        print(funcionario)
+    else:
+        print("Funcionário não encontrado.")
+
+def listar_todos_funcionarios(session, funcionario_id, **kwargs):
+    session.query(funcionario).filter_by(id=funcionario_id).update(kwargs)
+    session.commit()
+
+def pesquisar_um_funcionario(session, funcionario_id):
+    session.query(funcionario).filter_by(id=funcionario_id).delete()
+    session.commit()
+
+# Função para listar todos os funcionários
+def atualizar_funcionario(session):
+    funcionarios = session.query(funcionario).all()
+    for funcionario in funcionarios:
+        print(funcionario)
+
+
+
+
+
+
+
+#Função principal
+def main():
+    Session = sessionmaker(bind= MEU_BANCO)
+    session = Session()
+
+    while True:
+        print("\n=== RH System ===")
+        print("1 - Adicionar um funcionário")
+        print("2 - Consultar um funcionário")
+        print("3 - Atualizar os dados de um funcionário")
+        print("4 - Excluir um funcionário")
+        print("5 - Listar todos os funcionários")
+        print("0 - Sair do sistema.")
+
+        opcao = int(input("Digite a opção desejada: "))
+
+        if opcao == 1:
+            # Lógica para adicionar um funcionário
+            nome = input("Nome: ")
+            # ... outros dados
+            create_funcionario(session, nome, ...)
+        elif opcao == 2:
+            # Lógica para consultar um funcionário
+            funcionario_id = int(input("Digite o ID do funcionário: "))
+            read_funcionario(session, funcionario_id)
+        # ... outras opções
+
+        if opcao == 0:
+            break
+
+if __name__ == "__main__":
+    main()
